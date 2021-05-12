7af4d81a45f95c1ccf38149436487a6d80b3b7ff
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-05-12 13:59:05 +0100

    Update js.js

--- js.js
+++ js.js
@@ -6,6 +6,20 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  var sun = document.querySelector('.sun');
+
+  sun.addEventListener('click', () => {
+  document.body.classList.toggle('darkmode');
+  document.body.style.transition="all 1s";
+  if(document.body.classList.contains('darkmode')){
+    sun.innerHTML = "<i class='fas fa-moon'></i>";
+    document.querySelector('.aboutme').style.backgroundColor="rgb(69, 68, 68)";
+  }else{
+      sun.innerHTML = "<i class='fas fa-sun'></i>";
+      document.querySelector('.aboutme').style.backgroundColor="darkcyan";
+  }
+  });
+
 
   var myVar;
 
