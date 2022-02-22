09f17ad1752afc114286ce3f215ad9d536df1c2e
knight196 <71406824+knight196@users.noreply.github.com>
2022-02-22 15:56:15 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,57 +1,39 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function myFunction(x) {
+  x.classList.toggle("change");
+} 
 
-  var sun = document.querySelector('.sun');
+var sun = document.querySelector('.theme');
 
-  sun.addEventListener('click', () => {
-  document.body.classList.toggle('darkmode');
-  document.body.style.transition="all 1s";
-  if(document.body.classList.contains('darkmode')){
-    sun.innerHTML = "<i class='fas fa-moon'></i>";
-    document.querySelector('.aboutme').style.backgroundColor="rgb(69, 68, 68)";
-  }else{
-      sun.innerHTML = "<i class='fas fa-sun'></i>";
-      document.querySelector('.aboutme').style.backgroundColor="darkcyan";
-  }
-  });
-
-
-  var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+sun.addEventListener('click', () => {
+document.body.classList.toggle('darkmode');
+document.body.style.transition="all 1s";
+if(document.body.classList.contains('darkmode')){
+  sun.innerHTML = "<i class='bi bi-moon'></i>";
+  document.querySelector('.about-me').style.background="rgb(69, 68, 68);"
+}else{
+  sun.innerHTML = "<i class='bi bi-sun'></i>";
+  document.querySelector('.about-me').style.background="darkcyan";
 }
+});
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-
+// scroll btn
 //Get the button
-var mybutton =
-document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
-//when the user scrolls down 20px from the top of the document,show the button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-window.onscroll = function()
-{scrollFunction()};
-
-function scrollFunction(){
-  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-  {
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
-  }else{
+  } else {
     mybutton.style.display = "none";
   }
 }
 
-//when the user clicks on the button, scroll to the top of the document
-function topFunction(){
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
